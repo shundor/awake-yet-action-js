de4ff6b0f2676618e8cca181ebdb408ba0675be0
Matt Breton <breton16@github.com>
2022-12-22 14:38:46 -0500

    More

--- dist/index.js
+++ dist/index.js
@@ -9693,12 +9693,33 @@ async function run() {
     console.log("hello world!")
     const repoOwner = github.context.repo.owner;
     const repo = github.context.repo.repo;
-    const actor = github.context.actor;    
-    console.log(`found: ${repoOwner} ${repo} ${actor}!`);    
+    const actor = github.context.actor;
+    console.log(`found: ${repoOwner} ${repo} ${actor}!`);
+    const expected_events = ['opened', 'edited', 'reopened', 'created', 'submitted'];
+    console.log("Full event\n" + github.event)
+    console.log("Action:\n" + github.event.action)
+    console.log("Issue:\n" + github.event.issue)
+    if (expected_events.includes(github.event.action) && github.event.issue) {
+      // Issue details
+      console.log('Conditional for payload fired - issues')
+      const action = github.event.payload.issue.action
+      user = github.event.payload.issue.user
+      body = github.event.payload.issue.body
+      issue_number = github.event.payload.issue.number
+    } else if (expected_events.includes(github.event.payload.action) && github.event.payload.pull_request) {
+      // Pull Request details
+      console.log('Conditional for payload fired - pull_request')
+      const action = github.event.payload.pull_request.action
+      user = github.event.payload.pull_request.user
+      body = github.event.payload.pull_request.body
+    }
+
   } catch (error) {
     core.setFailed(error.message);
+
   }
 }
+
 
 run();
 
--- index.js
+++ index.js
@@ -7,31 +7,32 @@ async function run() {
     console.log("hello world!")
     const repoOwner = github.context.repo.owner;
     const repo = github.context.repo.repo;
-    const actor = github.context.actor;    
-    console.log(`found: ${repoOwner} ${repo} ${actor}!`);    
+    const actor = github.context.actor;
+    console.log(`found: ${repoOwner} ${repo} ${actor}!`);
+    const expected_events = ['opened', 'edited', 'reopened', 'created', 'submitted'];
+    console.log("Full event\n" + github.event)
+    console.log("Action:\n" + github.event.action)
+    console.log("Issue:\n" + github.event.issue)
+    if (expected_events.includes(github.event.action) && github.event.issue) {
+      // Issue details
+      console.log('Conditional for payload fired - issues')
+      const action = github.event.payload.issue.action
+      user = github.event.payload.issue.user
+      body = github.event.payload.issue.body
+      issue_number = github.event.payload.issue.number
+    } else if (expected_events.includes(github.event.payload.action) && github.event.payload.pull_request) {
+      // Pull Request details
+      console.log('Conditional for payload fired - pull_request')
+      const action = github.event.payload.pull_request.action
+      user = github.event.payload.pull_request.user
+      body = github.event.payload.pull_request.body
+    }
+
   } catch (error) {
     core.setFailed(error.message);
-  }
 
-  // Events the action is looking for
-  const expected_events= ['opened', 'edited', 'reopened', 'created', 'submitted'];
-  console.log("Full event\n" + github.event)
-  console.log("Action:\n" + github.event.action)
-  console.log("Issue:\n" + github.event.issue)
-  // if (expected_events.includes(github.event.action) && github.event.issue) {
-  //   // Issue details
-  //   console.log('Conditional for payload fired - issues')
-  //   const action = github.event.payload.issue.action
-  //   user = github.context.payload.issue.user
-  //   body = github.context.payload.issue.body
-  //   issue_number = github.context.payload.issue.number
-  // } else if (expected_events.includes(github.context.payload.action) && github.context.payload.pull_request) {
-  // // Pull Request details
-  //   console.log('Conditional for payload fired - pull_request')
-  //   const action = github.context.payload.pull_request.action
-  //   user = github.context.payload.pull_request.user
-  //   body = github.context.payload.pull_request.body
-  // };
+  }
 }
+
 
 run();
